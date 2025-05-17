b865f368d3637b478b7dfbf7cecb747366efc4d9
ekintekturk <ekintekturk@gmail.com>
2025-05-17 15:28:47 +0300

    Add files via upload

---
+++ Assets/script/Trail.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trail : MonoBehaviour
+{
+    [SerializeField] ParticleSystem dustParticles;
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Ground")
+        {
+            dustParticles.Play();
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Ground")
+        {
+            dustParticles.Stop() ;
+        }
+    }
+   
+
+
+
+
+
+}
